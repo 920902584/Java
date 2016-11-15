5558c9a8dfa1dca216efa4a4d00bc9e496edbd5e
920902584 <920902584@qq.com>
2016-11-15 18:52:12 +0800

    讲述了List和Set基本语法和用法

---
+++ Demo5-List-Set/HashSetText.java
@@ -0,0 +1,47 @@
+﻿package com.hashset.zx;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class HashSetText {
+
+	/**
+	 * @param args
+	 * HashSet：哈希表是通过使用称为散列法的机制来存储信息的，元素并没有以某种特定顺序来存放；  
+     * LinkedHashSet：以元素插入的顺序来维护集合的链接表，允许以插入的顺序在集合中迭代；  
+     * TreeSet：提供一个使用树结构存储Set接口的实现，对象以升序顺序存储，访问和遍历的时间很快。 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        HashSet<String> hs = new HashSet<String>();
+        hs.add("B");
+        hs.add("C");
+        hs.add("F");
+        hs.add("A");
+        hs.add("D");
+        hs.add("G");
+        hs.add("E");
+        System.out.println("HashSet输出顺序\n"+hs);      //输出顺序随机，与添加顺序无关
+        LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+        lhs.add("B");
+        lhs.add("C");
+        lhs.add("F");
+        lhs.add("A");
+        lhs.add("D");
+        lhs.add("G");
+        lhs.add("E");
+        System.out.println("LinkedHashSet输出顺序\n"+lhs);  //添加的顺序与输出的顺序不改变
+        TreeSet<String> ts =new TreeSet<String>();
+        ts.add("B");
+        ts.add("C");
+        ts.add("F");
+        ts.add("A");
+        ts.add("D");
+        ts.add("G");
+        ts.add("E");
+        System.out.println("TreeSet输出顺序\n"+ts);     //会按对象的升序顺序储存输出
+        
+	}
+
+}
---
+++ Demo5-List-Set/IteratorTest.java
@@ -0,0 +1,32 @@
+﻿package com.iterator.zx;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class IteratorTest {
+
+	/**
+	 * 熟悉一个ArrayList的用法。
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        List<String> book = new ArrayList<String>();
+        book.add("C语言");
+        book.add("数据结构");
+        book.add("JAVA开发实战经典");
+        ListIterator<String> iter =book.listIterator();   //实例化 Listlterator接口
+        System.out.print("遍历后输出为：");
+        //进行Iterator的遍历
+        while(iter.hasNext()){	
+        	//取出内容
+        String str =iter.next();
+        System.out.print(str+"、");
+        if(str.equals("数据结构")){
+        	iter.remove();
+        }
+	} 
+        System.out.println("");
+        System.out.println("删除后的集合为"+book);
+	}
+}
